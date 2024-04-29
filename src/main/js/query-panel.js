64f1011a0bb4056c431a28791ba205e309612f67
Nikita Dukin <kir.dukin@gmail.com>
2024-04-29 22:53:16 +0200

    feat(*): add interface

--- src/main/js/query-panel.js
+++ src/main/js/query-panel.js
@@ -1,13 +1,27 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 import { Button } from "antd"
 import { Input } from 'antd'
 
 export const QueryPanel = () => {
+    const [link, setLink] = useState("")
+    const [token, setToken] = useState("")
+    const [keyword, setKeyword] = useState("")
+
+    const handleChange = (method) => (e) => {
+        method(() => e.currentTarget.value)
+    }
+
     return <>
-        <Input placeholder="Basic usage" onChange={() => {}}/>
-        <Button type="primary">
-            Loading
+        <Input placeholder="GitHub organization link" value={link} onChange={handleChange(setLink)}/>
+        <Input placeholder="GitHub access token" value={token} onChange={handleChange(setToken)}/>
+        <Input placeholder="Keyword" value={keyword} onChange={handleChange(setKeyword)}/>
+        <Button type="primary" onClick={() => {
+            console.log(link)
+            console.log(token)
+            console.log(keyword)
+        }}>
+            Go
         </Button>
     </>;
 }
